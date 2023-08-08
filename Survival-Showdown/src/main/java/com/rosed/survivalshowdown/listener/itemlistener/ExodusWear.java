1d82de8c038bec953552bd126ae60ac7a7560573
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2023-08-07 23:57:32 -0400

    Added Exodus Logic
    - Heals for 2 seconds every 2.5 seconds. Activated by a melee hit

--- Survival-Showdown/src/main/java/com/rosed/survivalshowdown/listener/itemlistener/ExodusWear.java
+++ Survival-Showdown/src/main/java/com/rosed/survivalshowdown/listener/itemlistener/ExodusWear.java
@@ -1,10 +1,37 @@
 package com.rosed.survivalshowdown.listener.itemlistener;
 
 import com.rosed.survivalshowdown.util.ItemUtil;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.HashMap;
 
 public class ExodusWear extends ItemUtil implements Listener {
-    
+
+    private HashMap<Player, Long> exodusCooldownMap;
+    private final long exodusCooldown = 2500;
+
+    public ExodusWear()   {
+
+        exodusCooldownMap = new HashMap<>();
+
+    }
+
+    @EventHandler
+    public void onExodusHit(EntityDamageByEntityEvent e)   {
+
+        if (!(playerHitPlayerCheck(e.getEntity(), e.getDamager(), e.getCause())))   return;
+        Player attacker = (Player) e.getDamager();
+        if (checkCooldown(attacker, exodusCooldownMap, exodusCooldown) &&
+        itemCheck(attacker.getInventory().getHelmet(), "survivalShowdown.exodus"))   {
+            attacker.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 40, 0, true, false, false));
+            setCooldown(attacker, exodusCooldownMap);
+        }
+
+    }
 
 }
--- Survival-Showdown/src/main/java/com/rosed/survivalshowdown/util/ItemUtil.java
+++ Survival-Showdown/src/main/java/com/rosed/survivalshowdown/util/ItemUtil.java
@@ -1,6 +1,11 @@
 package com.rosed.survivalshowdown.util;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
 
 public abstract class ItemUtil {
 
@@ -18,6 +23,40 @@ public abstract class ItemUtil {
 
         return localizedName.equals(localizedNameChecker);
 
+    }
+
+    protected boolean playerHitPlayerCheck (Entity defender, Entity attacker, EntityDamageEvent.DamageCause damageCause)  {
+        boolean playerAttackedPlayer = true;
+
+        // check if the deffending entity is not living
+        if (!(defender instanceof Player))  {
+            playerAttackedPlayer = false;
+        }
+
+        // check if attacker isn't a player
+        if (!(attacker instanceof Player))  {
+            playerAttackedPlayer = false;
+        }
+
+        // check if cause is thorns
+        if (damageCause == EntityDamageEvent.DamageCause.THORNS) {
+            playerAttackedPlayer = false;
+        }
+
+        return playerAttackedPlayer;
+    }
+
+    protected boolean checkCooldown(Player player, HashMap<Player, Long> cooldowns, Long cooldown) {
+
+        if (!cooldowns.containsKey(player) || cooldowns.get(player) + cooldown <= System.currentTimeMillis()) {
+            return true;
+        }
+        return false;
+
+    }
+
+    protected void setCooldown(Player player, HashMap<Player, Long> cooldowns) {
+        cooldowns.put(player, System.currentTimeMillis());
     }
 
 }
